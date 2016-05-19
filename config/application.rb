cd398e2d7a1ab985142cb3952111ce4854769991
Robin Bhadran <robin.b@bridgeconn.com>
2016-05-19 12:09:04 +0530

    devise email conformation

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Multi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false  
+
+    ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "[username]",
+   :password => "[password]"
+ }
   end
 end
